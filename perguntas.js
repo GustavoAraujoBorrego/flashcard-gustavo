88d5617cba07789f1fdbf35fa25359205c78a79a
GustavoAraujoBorrego <souza.araujo.gustavo@escola.pr.gov.br>
2025-08-06 12:25:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Quem é o time mais mais rico do brasil?',
+    'O time mais rico do brasil é o FLAMENGO'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual Seleção que foi campeão do mundo em 2002 no japão?',
+    'O Brasil foi penta campeão do mundo em 2002 com cafú como capitão do penta'
+)
+
+criaCartao(
+    'Futebol',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Futebol',
+    'Em que ano o Flamengo?',
+    'Oi em ingles é HI (RAI)'
+)
